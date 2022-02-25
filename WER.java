a80c5055aba2fe0e072f6abb02ac33fc719f4b2d
Darya <dashkash22@mail.ru>
2022-02-25 22:51:46 +0300

    Teeest

---
+++ WER.java
@@ -0,0 +1,42 @@
+package QWER11;
+public class WER {
+    public static void main (String[] args){
+    for (int i=1; i<11 ;i++){
+        switch (i) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Tree");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+            case 11:
+                System.out.println("Eleven");
+                break;
+        }
+    }
+}
+}
